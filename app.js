993eaad4ed1e9f2e54a50488ee3c2e09c8da67aa
pajju <prajwalshah.2003@gmail.com>
2023-01-13 20:50:06 +0530

    adding cors

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 const trainRoutes = require('./routes/trainRoutes');
-
+app.use(cors());
 //middlewares
 app.use(express.json());
 app.use('/api/v1/trains',trainRoutes);
